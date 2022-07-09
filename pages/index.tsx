acfee954b96a530a55d9ab2932f9b6a27220d003
winchesHe <329487092@qq.com>
2022-07-09 17:08:44 +0800

    feat: 新增axios网络请求模块

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,21 @@ import type { NextPage } from 'next'
 import styled from 'styled-components'
 import styles from '../styles/Home.module.css'
 import { Button } from 'antd'
+import axios from 'axios'
 
 const Home: NextPage = () => {
+  async function send() {
+    const res = await axios.get('http://localhost:3000');
+    console.log(res);
+  }
+  send()
+
   return (
     <div className={styles.container}>
       <Wrapper>
         <Title>Hello World, this is my first styled component!</Title>
       </Wrapper>
-      <Button type="primary">Primary Button</Button>
+    <Button type="primary">Primary Button</Button>
     <Button>Default Button</Button>
     </div>
   )
